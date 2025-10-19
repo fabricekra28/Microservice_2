8a4e33aedf60f7dfdb865f0aa78b27b9bdba2ac7
fabricekra28 <60808077+fabricekra28@users.noreply.github.com>
2025-10-19 13:46:21 +0000

    Update start.sh

--- user/start.sh
+++ user/start.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 echo "Waiting for PostgreSQL..."
-while ! nc -z db.microservices-app2..svc.cluster.local 5432; do
+while ! nc -z db.microservices-app2.svc.cluster.local 5432; do
   echo "PostgreSQL is unavailable - sleeping"
   sleep 2
 done
