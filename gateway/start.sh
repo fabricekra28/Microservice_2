86d3327e81a9e7fb75048fa7e624a18320ab875f
fabricekra28 <krafabrice64@gmail.com>
2025-10-19 00:14:55 +0000

    add Microservice

--- gateway/start.sh
+++ gateway/start.sh
@@ -3,19 +3,19 @@
 echo "Waiting for backend services..."
 
 # On attend que users_service soit joignable
-while ! nc -z user 8008; do
+while ! nc -z user.microservices-app2.svc.cluster.local  8008; do
   echo "Waiting for users_service..."
   sleep 2
 done
 
 # On attend que products_service soit joignable
-while ! nc -z maison 8005; do
+while ! nc -z maison.microservices-app2.svc.cluster.local  8005; do
   echo "Waiting for maison..."
   sleep 2
 done
 
 # On attend que orders_service soit joignable
-while ! nc -z location 8006; do
+while ! nc -z location.microservices-app2.svc.cluster.local  8006; do
   echo "Waiting for location_service..."
   sleep 2
 done
